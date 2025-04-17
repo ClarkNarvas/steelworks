5dd6d1d934f51ae70a825f0ccf050237322137d6
Clark Narvas <clarknarvas13@gmail.com>
2025-04-17 21:23:14 +0100

    test to work on prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
-import type React from "react"
+import type { ReactNode } from "react"
 import NavigationBar from "../components/navigation-bar"
 import "./globals.css"
+import type { Metadata } from "next"
 
+export const metadata: Metadata = {
+  title: "Steelworks",
+  description: "Your site description",
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
