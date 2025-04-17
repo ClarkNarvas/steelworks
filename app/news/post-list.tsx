e7f3ba39590251a7fb8d07aa6e9650a081615c78
Clark Narvas <clarknarvas13@gmail.com>
2025-04-17 19:34:50 +0100

    post name shows up

--- app/news/post-list.tsx
+++ app/news/post-list.tsx
@@ -1,17 +1,21 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-export default function PostList(props) {
+function PostList({ data }) {
+  const posts = data.postConnection.edges.map(edge => ({
+    title: edge.node.title,
+    slug: edge.node._sys.filename,
+    date: edge.node.date,
+  }));
+  posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
   return (
-
-
-<div className="max-w-2xl mx-auto p-4">
-      <h1 className="text-3xl mb-8 text-center mt-3">News</h1>
+    <div className="max-w-2xl mx-auto p-4">
+      <h1 className="text-3xl mb-8 text-center mt-3">Posts</h1>
       <ul>
-      {props.data.postConnection.edges.map((post) => (
-          <li key={post.slug} className=" rounded-lg p-4 border-b group transition-shadow">
-            <Link href={`/news/${post.node._sys.filename}`} className="block">
-              <h2 className="text-3xl m-0 mb-2 group-hover:stroke-pink-600">{post.node._sys.filename}</h2>
-              <p className="text-gray-600 mt-5">Date Here</p>
+        {posts.map(post => (
+          <li key={post.slug} className="rounded-lg p-4 border-b group transition-shadow">
+            <Link href={`/news/${post.slug}`} className="block">
+              <h2 className="text-3xl m-0 mb-2 group-hover:stroke-pink-600">{post.title}</h2>
+              <p className="text-gray-600 mt-5">{new Date(post.date).toLocaleDateString()}</p>
             </Link>
           </li>
         ))}
@@ -19,3 +23,5 @@ export default function PostList(props) {
     </div>
   );
 }
+
+export default PostList;
