f44ab8b64bad5a28386e2d5eab411f22f73dd55f
Clark Narvas <clarknarvas13@gmail.com>
2025-04-18 11:10:22 +0100

    sort desc

--- app/news/[...filename]/page.tsx
+++ app/news/[...filename]/page.tsx
@@ -3,7 +3,7 @@ import client from "../../../tina/__generated__/client";
 
 export async function generateStaticParams() {
   const pages = await client.queries.newsConnection({
-    sort: "date",
+    sort: "date:desc",
   })
 
   const paths = pages.data?.newsConnection?.edges?.map((edge) => ({
